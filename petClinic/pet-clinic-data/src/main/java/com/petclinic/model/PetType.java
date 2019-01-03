06933d9d1846693f75b43b05ba4a33b52de8f24e
12Ozlem <oz.aydogmus@gmail.com>
2019-01-03 11:39:50 +0300

    Initial Commit to master branch
    Signed-off-by: 12Ozlem <oz.aydogmus@gmail.com>

---
+++ petClinic/pet-clinic-data/src/main/java/com/petclinic/model/PetType.java
@@ -0,0 +1,30 @@
+package com.petclinic.model;
+
+public class PetType {
+
+	private String name;
+
+	public PetType() {
+	}
+
+	public PetType(String name) {
+		this.name = name;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	@Override
+	public String toString() {
+		return "PetType [name=" + name + "]";
+	}
+	
+	
+	
+	
+}
---
+++ petClinic/pet-clinic-data/src/main/java/com/petclinic/model/Vet.java
@@ -0,0 +1,6 @@
+package com.petclinic.model;
+
+public class Vet extends Person {
+
+	
+}
