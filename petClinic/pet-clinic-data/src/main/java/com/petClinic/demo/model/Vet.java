9178b0841b89355f4075ec04edb205839a5bb533
12Ozlem <oz.aydogmus@gmail.com>
2019-01-16 13:22:07 +0300

    Closes #23

--- petClinic/pet-clinic-data/src/main/java/com/petClinic/demo/model/Vet.java
+++ petClinic/pet-clinic-data/src/main/java/com/petClinic/demo/model/Vet.java
@@ -1,6 +1,31 @@
 package com.petClinic.demo.model;
 
+import java.util.Set;
+
 public class Vet extends Person {
 
+	Set <Speciality> spec;
+
+	public Vet() {
+
+	}
+
+	public Vet(Set<Speciality> spec) {
+		super();
+		this.spec = spec;
+	}
+
+	public Set<Speciality> getSpec() {
+		return spec;
+	}
+
+	public void setSpec(Set<Speciality> spec) {
+		this.spec = spec;
+	}
+
+	
+	
+	
+	
 	
 }
